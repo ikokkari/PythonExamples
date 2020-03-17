0920174756e122c8c0aa77854ed0c35797aa60a3
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2020-03-17 11:50:58 -0400

    Add files via upload

--- conditions.py
+++ conditions.py
@@ -1,7 +1,4 @@
 def sign(a):
-    """Calculate and return the sign of the integer parameter.
-    a -- The integer parameter whose sign is computed.
-    """
     if a < 0:
         return -1
     elif a > 0:
@@ -10,9 +7,6 @@ def sign(a):
         return 0
     
 def median(a, b, c):
-    """Find and return the median of the three given parameters.
-    a, b, c --- The three values whose median to compute.
-    """
     if a <= b <= c or c <= b <= a:
         return b
     elif b <= a <= c or c <= a <= b:
@@ -21,9 +15,6 @@ def median(a, b, c):
         return c
     
 def median_other_way(a, b, c):
-    """Find and return the median of the three given parameters.
-    a, b, c --- The three values whose median to compute.
-    """
     if a > b and a > c: # a is maximum of three
         return max(b, c)
     elif a < b and a < c: # a minimum of three
@@ -32,11 +23,6 @@ def median_other_way(a, b, c):
         return a  # the only possibility that remains
     
 def days_in_month(m, leap_year = False):
-    """Given the month as an integer, compute how many days there
-    are in that month.
-    m -- The month number as an integer 1 to 12.
-    leap_year -- Whether the current year is a leap year.
-    """
     if m < 1 or m > 12:
         return 0
     elif m == 2:
@@ -50,9 +36,6 @@ def days_in_month(m, leap_year = False):
         return 31
     
 def is_leap_year(y):
-    """Determine whether the current year is a leap year.
-    y -- The current year.
-    """
     if y % 4 != 0:
         return False
     if y % 100 != 0:
@@ -62,9 +45,6 @@ def is_leap_year(y):
 # The same conditions written in a different way.
 
 def is_leap_year_another_way(y):
-    """Determine whether the current year is a leap year.
-    y -- The current year.
-    """
     if y % 400 == 0:
         return True
     if y % 100 == 0:
@@ -74,16 +54,9 @@ def is_leap_year_another_way(y):
 # The whole function as a Pythonesque one-liner.
 
 def is_leap_year_with_logic(y):
-    """Determine whether the current year is a leap year.
-    y -- The current year.
-    """
     return y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)
 
 def test_leap_year():
-    """Verify that all three functions give the same answer
-    for the years within those mentioned in the famous song
-    "In the Year 2525" by Zager & Evans.
-    """
     for y in range(2525, 9511):
         a1 = is_leap_year(y)
         a2 = is_leap_year_another_way(y)
