b25c96bfdca5f84796b2a86e9b6bdcf01f8d7a26
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2021-07-08 21:36:06 -0400

    Add files via upload
    Renamed local variables to be more descriptive of their purpose.

--- hangman.py
+++ hangman.py
@@ -3,11 +3,11 @@ import random
 
 def hangman(wordlist, blank='*'):
     misses, guesses = 0, []
-    word = random.choice(wordlist)
-    letters = [blank] * len(word)
+    secret = random.choice(wordlist)
+    displayed = [blank for _ in secret]
     print("Let us play a game of Hangman.")
-    while blank in letters:
-        print(f"Current word is {'-'.join(letters)}.")
+    while blank in displayed:
+        print(f"Current word is {'-'.join(displayed)}.")
         print(f"Letters guessed so far: {''.join(guesses)}")
         guess = input("Please enter a letter, or 'quit' to quit: ")
         if guess == 'quit':
@@ -16,22 +16,22 @@ def hangman(wordlist, blank='*'):
             guesses.append(guess)
             guesses.sort()
         hit = 0
-        for (i, (let, w)) in enumerate(zip(letters, word)):
-            if let == blank and w == guess:
-                letters[i] = guess
+        for (i, (c1, c2)) in enumerate(zip(displayed, secret)):
+            if c1 == blank and c2 == guess:
+                displayed[i] = guess
                 hit += 1
         if hit > 0:
             print(f"That one hit {hit} time{'s' if hit > 1 else ''}!")
         else:
             print("That one was a miss.")
             misses += 1
-    print(f"You guessed {word!r} with {misses} misses.\n")
+    print(f"You guessed {secret!r} with {misses} misses.\n")
     return misses
 
 
 if __name__ == "__main__":
     with open('words_sorted.txt', encoding="utf-8") as f:
-        wordlist = [word.strip() for word in f]
+        wordlist = [word.strip() for word in f if len(word) > 5]
     print(f"Read a word list of {len(wordlist)} words.")
     while True:
         if hangman(wordlist) is None:
