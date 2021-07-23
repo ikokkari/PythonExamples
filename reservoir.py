508e32c988493b8096daba5da8f33867d1cc507a
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2021-07-22 23:02:39 -0400

    Add files via upload

--- reservoir.py
+++ reservoir.py
@@ -13,28 +13,28 @@ from random import randint, shuffle
 # with for-loop, either way works and we don't need anything else.
 
 def reservoir(seq, k):
-    buffer = []
+    lounge = []
     for (count, v) in enumerate(seq):
         if count < k:  # First k elements build up the reservoir.
-            buffer.append(v)
+            lounge.append(v)
         else:
             idx = randint(0, count)  # Others take a random shot.
             if idx < k:  # The new element hits the reservoir.
-                buffer[idx] = v  # Displace some previous element.
-    # shuffle(buffer)  # Shuffle the buffer in place.
-    yield from buffer  # All done, so emit the reservoir.
+                lounge[idx] = v  # Displace some previous element.
+    shuffle(lounge)  # Shuffle the buffer in place.
+    yield from lounge  # All done, so emit the reservoir.
 
 
 # The shuffling step in the end can be removed if we don't care
-# about the order of elements in the sample, but just the subset
-# of elements chosen to the sample.
+# about the order of elements inside the sample, only about the
+# subset of elements that were chosen to the sample.
 
 def __demo():
     print("Here are 20 random non-short lines from 'War and Peace':")
     with open('warandpeace.txt', encoding='utf-8') as wap:
-        source = enumerate(reservoir((x.strip() for x in wap if len(x) > 60), 20))
-        for (idx, line) in source:
-            print(f"{idx:2}: {line}")
+        chosen = enumerate(reservoir((line for line in wap if len(line) > 60), 20))
+        for (idx, line) in chosen:
+            print(f"{idx:2}: {line}", end='')
 
 
 if __name__ == "__main__":
