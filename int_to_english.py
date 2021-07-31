cbaff054c3364dd21668613ecaeb5b6ab0813b5c
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2021-07-30 21:27:12 -0400

    Add files via upload

--- int_to_english.py
+++ int_to_english.py
@@ -1,25 +1,25 @@
 __googol = 10 ** 100
 
 # http://lcn2.github.io/mersenne-english-name/tenpower/tenpower.html
-__pows = (("thousand", 3), ("million", 6), ("billion", 9),
-          ("trillion", 12), ("quadrillion", 15), ("quintillion", 18),
-          ("sextillion", 21), ("septillion", 24), ("octillion", 27),
-          ("nonillion", 30), ("decillion", 33), ("undecillion", 36),
-          ("duodecillion", 39), ("tredecillion", 42),
-          ("quattuordecillion", 45), ("quindecillion", 48),
-          ("sexdecillion", 51), ("eptendecillion", 54),
-          ("octadecillion", 57), ("novemdecillion", 60),
-          ("vigintillion", 63), ("unvigintillion", 66),
-          ("duovigintillion", 69), ("trevigintillion", 72),
-          ("quattuorvigintillion", 75), ("quinvigintillion", 78),
-          ("sexvigintillion", 81), ("septenvigintillion", 84),
-          ("octavigintillion", 87), ("novemvigintillion", 90),
-          ("trigintillion", 93), ("untrigintillion", 96),
-          ("duotrigintillion", 99)
-          )
+__power_names = (("thousand", 3), ("million", 6), ("billion", 9),
+                 ("trillion", 12), ("quadrillion", 15), ("quintillion", 18),
+                 ("sextillion", 21), ("septillion", 24), ("octillion", 27),
+                 ("nonillion", 30), ("decillion", 33), ("undecillion", 36),
+                 ("duodecillion", 39), ("tredecillion", 42),
+                 ("quattuordecillion", 45), ("quindecillion", 48),
+                 ("sexdecillion", 51), ("eptendecillion", 54),
+                 ("octadecillion", 57), ("novemdecillion", 60),
+                 ("vigintillion", 63), ("unvigintillion", 66),
+                 ("duovigintillion", 69), ("trevigintillion", 72),
+                 ("quattuorvigintillion", 75), ("quinvigintillion", 78),
+                 ("sexvigintillion", 81), ("septenvigintillion", 84),
+                 ("octavigintillion", 87), ("novemvigintillion", 90),
+                 ("trigintillion", 93), ("untrigintillion", 96),
+                 ("duotrigintillion", 99)
+                 )
 
 # Dictionary comprehension, analogous to list comprehension.
-__pows = {p: n for n, p in __pows}
+__power_names = {p: n for n, p in __power_names}
 
 
 def __int_to_eng(n):
@@ -62,7 +62,7 @@ def int_to_english(n):
             if p == 0:
                 result.append(__int_to_eng(trip))
             else:
-                result.append(__int_to_eng(trip) + " " + __pows[p])
+                result.append(__int_to_eng(trip) + " " + __power_names[p])
         p += 3
     return " ".join(reversed(result))
 
@@ -71,9 +71,9 @@ def __int_to_english_demo():
     for x in [42, 3**7, 6**20, -(2**100), 9**200, 10**500 + 1]:
         print(f"{x} written in English is {int_to_english(x)}.")
     print("\nHere are integers 0-100 sorted in alphabetical order:")
-    print(sorted(range(0, 101), key=int_to_english))
+    print([int_to_english(n) for n in sorted(range(0, 101), key=int_to_english)])
     print("\nHere are integers 0-100 sorted by their name lengths:")
-    print(sorted(range(0, 101), key=lambda n: (len(int_to_english(n)), n)))
+    print([int_to_english(n) for n in sorted(range(0, 101), key=lambda n: (len(int_to_english(n)), n))])
     print("\nHere are integers 0-1000 whose name does not contain 'o':")
     print([x for x in range(1001) if 'o' not in int_to_english(x)])
 
