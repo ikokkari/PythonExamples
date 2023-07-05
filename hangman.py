5f2c45412a50a8482afa8a75ee70415cfb5f4429
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2023-07-04 21:41:54 -0400

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,29 +1,26 @@
 from random import Random
 
-rng = Random()
 
-
-def hangman(wordlist, blank='*'):
-    misses, guesses = 0, []
-    secret = rng.choice(wordlist)
-    displayed = [blank for _ in secret]
-    print("Let us play a game of Hangman.")
-    while blank in displayed:
-        print(f"Current word is {'-'.join(displayed)}.")
-        print(f"Letters guessed so far: {''.join(guesses)}")
+def hangman(secret, blank='*'):
+    misses, guessed = 0, []
+    board = [blank for _ in secret]
+    print("All righty, let us play a game of Hangman.")
+    while blank in board:
+        print(f"Current word is {'-'.join(board)}.")
+        print(f"Letters guessed so far: {''.join(guessed)}")
         guess = input("Please enter a letter, or 'quit' to quit: ")
         if guess == 'quit':
             return None
-        if guess not in guesses:
-            guesses.append(guess)
-            guesses.sort()
+        if guess not in guessed:
+            guessed.append(guess)
+            guessed.sort()
         hit = 0
-        for (i, (c1, c2)) in enumerate(zip(displayed, secret)):
-            if c1 == blank and c2 == guess:
-                displayed[i] = guess
+        for (pos, (board_char, secret_char)) in enumerate(zip(board, secret)):
+            if board_char == blank and secret_char == guess:
+                board[pos] = guess
                 hit += 1
         if hit > 0:
-            print(f"That one hit {hit} time{'s' if hit > 1 else ''}!")
+            print(f"That one hit {hit} {'times' if hit > 1 else 'time'}!")
         else:
             print("That one was a miss.")
             misses += 1
@@ -32,11 +29,13 @@ def hangman(wordlist, blank='*'):
 
 
 def __demo():
-    with open('words_sorted.txt', encoding="utf-8") as f:
-        wordlist = [word.strip() for word in f if len(word) > 5]
-    print(f"Read a word list of {len(wordlist)} words.")
+    # The text file words_sorted.txt contains one word per line.
+    with open('words_sorted.txt', encoding="utf-8") as word_file:
+        wordlist = [line.strip() for line in word_file if len(line) > 5]
+    print(f"Finished reading a word list of {len(wordlist)} words.")
+    rng = Random()  # RNG seed is taken from system clock, different each run
     while True:
-        if hangman(wordlist) is None:
+        if hangman(rng.choice(wordlist)) is None:
             print("See you around!")
             break
 
--- mathproblems.py
+++ mathproblems.py
@@ -1,8 +1,9 @@
 from random import Random
 
 
-# The oldest algorithm in recorded history, Euclid's algorithm
-# finds the greatest common divisor of two nonnegative integers.
+# The oldest algorithm in recorded history, Euclid's GCD algorithm
+# finds the greatest common divisor of two non-negative integers.
+# (This function is already in the math module as math.gcd.)
 
 def euclid_gcd(a, b, verbose=False):
     while b > 0:
@@ -12,21 +13,18 @@ def euclid_gcd(a, b, verbose=False):
     return a
 
 
-# This algorithm can be extended to find two numbers x and y
-# that satisfy ax + by = gcd(a, b), which will be useful in
-# various mathematical algorithms.
+# This algorithm could even be extended to find two numbers x and y
+# that satisfy ax + by = gcd(a, b), which will be useful in various
+# mathematical algorithms.
 
-# Generate the Collatz series from the given starting value.
+# Generate the Collatz 3n+1 series from the given starting value.
 # https://en.wikipedia.org/wiki/Collatz_conjecture
 
 def collatz(start):
-    curr, result = start, []
-    if curr < 1:
-        return []
-    while curr > 1:
-        result.append(curr)
-        curr = curr // 2 if curr % 2 == 0 else 3 * curr+1
-    result.append(1)
+    result = [start]
+    while result[-1] > 1:
+        n = result[-1]
+        result.append(2 // n if n % 2 == 0 else 3*n + 1)
     return result
 
 
@@ -58,6 +56,75 @@ def iterate_diff(items, verbose=False):
 # number of elements than only four? Would the convergence of
 # this system still be guaranteed? Curious students might want
 # to investigate this phenomenon.
+
+
+# Another algorithm from the ancient world, Heron's square root
+# method to numerically iterate the guess for the square root of
+# the positive real number x. (This algorithm generalizes to any
+# roots, and in fact turns out to be special case of Newton's
+# numerical iteration with the function whose root we want to
+# solve hardcoded to square root.)
+
+def heron_root(x):
+    if x < 0:
+        raise ValueError("Square roots of negative numbers are not allowed")
+    guess, prev = x / 2, 0
+    # Fingers crossed, this will converge in float precision.
+    while guess != prev:
+        prev = guess
+        guess = (guess + x / guess) / 2
+    return guess
+
+
+# Converting Roman numerals and positional integers back and forth
+# makes for interesting and educational example of loops, lists
+# and dictionaries.
+
+symbols_encode = [
+    (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+    (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+]
+
+
+def roman_encode(n):
+    if n < 1:
+        raise ValueError("Can't convert {n}; Romans did not know zero or negative numbers")
+    result = ''
+    for (v, s) in symbols_encode:
+        while v <= n:  # same symbol can be used several times
+            result += s
+            n -= v
+    return result
+
+
+# Dictionaries map symbols into the values that they encode.
+
+symbols_decode = {
+    'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
+}
+
+
+def roman_decode(s):
+    result, prev = 0, 0
+    for c in reversed(s.upper()):  # Loop right to left
+        v = symbols_decode[c]
+        result += (v if prev <= v else -v)
+        prev = v
+    return result
+
+
+# Whenever two functions are each other's inverses, it is easy to
+# test them by looping through some large number of possible inputs
+# and verify that both functions invert the result produced by
+# the other. This does not guarantee these functions to be correct,
+# though, just that if they are wrong, they both are wrong in the
+# same particular way.
+
+def test_roman():
+    for n in range(1, 5000):
+        if n != roman_decode(roman_encode(n)):
+            return False
+    return True
 
 
 # Whenever a problem is stated in terms of integers, you should aim
@@ -103,83 +170,16 @@ def integer_root(n, k=2):
     return a
 
 
-# Another algorithm from the ancient world, Heron's square root
-# method to numerically iterate the guess for the square root of
-# the positive real number x. (This algorithm generalizes to any
-# roots, and in fact turns out to be special case of Newton's
-# numerical iteration with the function whose root we want to
-# solve hardcoded to square root.)
-
-def heron_root(x):
-    if x < 0:
-        raise ValueError("Square roots of negative numbers not allowed")
-    guess, prev = x / 2, 0
-    while guess != prev:  # This will converge in float precision.
-        prev, guess = guess, (guess + x / guess) / 2
-    return guess
-
-
-# Converting Roman numerals and positional integers back and forth
-# makes for interesting and educational example of loops, lists
-# and dictionaries.
-
-symbols_encode = [
-    (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-    (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
-]
-
-
-def roman_encode(n):
-    if n < 1:
-        raise ValueError("Romans did not have zero or negative numbers")
-    result = ''
-    for (v, s) in symbols_encode:
-        while v <= n:  # same symbol can be used several times
-            result += s
-            n -= v
-    return result
-
-
-# Dictionaries map symbols into the values that they encode.
-
-symbols_decode = {
-    'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
-}
-
-
-def roman_decode(s):
-    result, prev = 0, 0
-    for c in reversed(s):  # Loop right to left
-        v = symbols_decode[c]
-        result += (v if prev <= v else -v)
-        prev = v
-    return result
-
-
-# Whenever two functions are each other's inverses, it is easy to
-# test them by looping through some large number of possible inputs
-# and verify that both functions invert the result produced by
-# the other. This does not guarantee these functions to be correct,
-# though, just that if they are wrong, they both are wrong in the
-# same particular way.
-
-def test_roman():
-    for n in range(1, 5000):
-        if n != roman_decode(roman_encode(n)):
-            return False
-    return True
-
-
 def __demo():
-    a, b = 2*3*3*13*17*49, 3*5*5*7*33*19
-    print(f"Greatest common divisor of {a} and {b}, visualized:")
+    a, b = 2*3*3*13*17*49, 3*5*5*7*19*33
+    print(f"Greatest common divisor of {a} and {b}, verbose:")
     euclid_gcd(a, b, verbose=True)
     print(f"\nRoman numbers conversion works? {test_roman()}")
     print(f"\nHeron square root of 2 equals {heron_root(2)}.")
 
     print("\nHere are some Collatz sequences.")
     for n in [17, 100, 1234567]:
-        print(f"{n} goes to 1 with {collatz(n)}")
+        print(f"{n} reaches 1 with the steps {collatz(n)}")
 
     print("\nHere are the convergences of some four-lists:")
     rng = Random(12345)
@@ -197,9 +197,9 @@ def __demo():
     n = 123**456
     s = str(integer_root(n))
     print(f"Integer square root of 123**456 has {len(s)} digits.")
-    print(f"First five are {s[:5]}, and last five are {s[-5:]}.")
+    print(f"First five digits are {s[:5]}, and last five digits are {s[-5:]}.")
 
-    # How many numbers are longer written in Arabic than in Roman?
+    # Which integers are longer written in Arabic than in Roman?
     shorter = [x for x in range(1, 5000) if len(str(x)) > len(roman_encode(x))]
     shorter = ', '.join([f"{x} ({roman_encode(x)})" for x in shorter])
     print(f"\nNumbers longer in Arabic than Roman: {shorter}")
